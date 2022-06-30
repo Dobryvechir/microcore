2ee32121235fec27405fc9c3c69657b8cc464295
Dobryvechir <ddobryvechir@outlook.com>
2022-06-30 13:29:53 +0200

    oracle driver as sample only

--- build/ci/reinstall.cmd
+++ build/ci/reinstall.cmd
@@ -1,5 +1,5 @@
 go install github.com/Dobryvechir/microcore/pkg/main
 @del %GOPATH%\bin\microcore.exe
 @ren %GOPATH%\bin\main.exe microcore.exe
-@copy %GOPATH%\bin\microcore.exe C:\Go\bin\microcore.exe
+@copy %GOPATH%\bin\microcore.exe C:\prg\go\bin\microcore.exe
 
